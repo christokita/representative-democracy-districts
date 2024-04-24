c2ddc9aa8571dd115cbd94354c460f697bd62af0
Chris Tokita <christopher.tokita@gmail.com>
2024-04-24 18:32:27 -0500

    Update model.py

--- scripts/schelling_model/model.py
+++ scripts/schelling_model/model.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap, to_hex
+from matplotlib.colors import ListedColormap
 import numpy as np
 import random
-import scipy as sp
+from scipy import signal
 from typing import List
 
 
@@ -11,19 +11,36 @@ class SchellingModel:
     Class for our Schelling model of social segregation.
     """
 
-    def __init__(self, dimensions: List[int], proportions: List[float], vacancy: float) -> None:
+    def __init__(self, dimensions: List[int], proportions: List[float], vacancy: float, satisfaction_threshold: float) -> None:
         """
         Parameters
-            dimensions:     desired dimensions of grid for population size List[0] x List[1].
-            proportions:    desired proportions for different identities in population.
-            vacancy:        % of spots on grid vacant.
+            dimensions:                 desired dimensions of grid for population size List[0] x List[1].
+            proportions:                desired proportions for different identities in population.
+            vacancy:                    % of spots on grid vacant.
+            satisfaction_threshold:     minimum desired % of neighbors that are the same identity.
 
         Returns:
             None
         """
+        # Model parameters
         self.dimensions = dimensions
         self.population_proportions = proportions
+        self.vacancy = vacancy
+        self.threshold = satisfaction_threshold
+
+        # Set up model grid
         self.grid = self.populate_grid(dimensions=dimensions, proportions=np.array(proportions), vacancy=vacancy)
+        self.grid_initial = None
+
+        # Simulation variables for tracking purposes
+        self.t_end = None #what timestep does simulation end on
+
+        # Neighborhood kernel for each individual on the grid
+        self.kernel = np.array([
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]
+        ])
 
     def populate_grid(self, dimensions: List[int], proportions: np.array, vacancy: float) -> np.ndarray:
         """
@@ -71,9 +88,142 @@ class SchellingModel:
         random.shuffle(spot_values)
         return np.reshape(spot_values, newshape=tuple(dimensions))
     
-    def show_grid(self, vacant_color: str = "white", colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"]) -> None:
+    def evolve(self, max_steps: int = 1000, boundary = "wrap") -> None:
+        """
+        Simulate Schelling model on the population grid.
+
+        Parameters:
+            max_steps:  maximum number of steps to run model.
+            boundary:   how boundaries are dealt with on the grid.
+
+        Returns:
+            None (this updates self.grid)
+        """
+        self.grid_initial = self.grid.copy()
+
+        # Grab unique types in the grid
+        types = range(len(self.population_proportions))
+
+        # Iterate until all individuals are satsfied or we run out of steps
+        step = 0
+        while step < max_steps:
+
+            # Calculate how many non-vacant neighbors each individual has
+            total_neighbors = signal.convolve2d(
+                self.grid != -1, 
+                self.kernel,
+                mode="same",
+                boundary=boundary
+            )
+
+            # Count up how many in-group neighbors each individual has
+            type_neighbors = {}
+            for t in types:
+                type_neighbors[t] = signal.convolve2d(
+                    self.grid == t, 
+                    self.kernel,
+                    mode="same",
+                    boundary=boundary
+                ) 
+
+            # Find dissatisfied individuals
+            dissatisfied = np.zeros_like(self.grid, dtype=bool)
+            type_dissatisfied = {}
+            for t in types:
+                type_dissatisfied[t] = (type_neighbors[t]/total_neighbors < self.threshold) & (self.grid == t)
+                dissatisfied |= type_dissatisfied[t]
+
+            if not dissatisfied.any():
+                break #stop iterating if all individuals are satisfied
+
+            # Allow all dissatisfied individuals to move randomly
+            vacant_spots = np.sum(self.grid == -1)
+            self.grid[dissatisfied] = -1
+            
+            filling = -np.ones(vacant_spots)
+            for t in types:
+                n_movers = type_dissatisfied[t].sum()
+                filling = np.append(filling, np.repeat(t, n_movers))
+
+            np.random.shuffle(filling)
+            self.grid[self.grid == -1] = filling
+
+            # Update step
+            step += 1
+        
+        print(f"Model simulation done at t = {step:,}")
+        self.t_end = step
+        return None
+    
+
+    def _plot_grid(
+            self, stage: str = "current", vacant_color: str = "white", colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"], ax: plt.axes = None
+        ) -> None:
         """
         Visualize populated grid.
+
+        Parameters:
+            stage:          "current" or "initial" grid
+            vacant_color:   color for empty spots.
+            colors:         list of colors for occupied spots.
+            ax:             matplotlib axes object to plot on (optional).
+
+        Returns:
+            None
+        """
+        # Generate color map
+        if len(self.population_proportions) > len(colors):
+            raise("Not enough colors to plot.")
+        elif self.vacancy != 0:
+            colors = colors[0:len(self.population_proportions)]
+            colors.insert(0, vacant_color)
+        else:
+            colors = colors[0:len(self.population_proportions)]
+
+        # Select grid to display
+        if stage == "current":
+            grid = self.grid
+        elif stage == "initial":
+            grid = self.grid_initial
+
+        # Plot grid
+        if ax is None:
+            _, ax = plt.subplots()
+        ax.imshow(
+            grid,
+            cmap=ListedColormap(colors)
+        )
+
+        # Format grid
+        ax.set_xticks([])
+        ax.set_yticks([])
+        for spine in ax.spines.values():
+            spine.set_linewidth(2)
+        return ax
+    
+    def show_grid(
+            self, stage: str = "current", vacant_color: str = "white", colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"]
+        ) -> None:
+        """
+        Visualize a stage of the populated grid.
+
+        Parameters:
+            stage:          "current" or "initial" grid
+            vacant_color:   color for empty spots.
+            colors:         list of colors for occupied spots.
+
+        Returns:
+            None
+        """
+        self._plot_grid(stage=stage, vacant_color=vacant_color, colors=colors)
+        plt.tight_layout()
+        plt.show()
+
+    def show_grid_evolution(
+            self, vacant_color: str = "white", colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"]
+        ) -> None:
+        """
+        Visualize grid before and after simulation.
 
         Parameters:
             vacant_color:   color for empty spots.
@@ -82,18 +232,32 @@ class SchellingModel:
         Returns:
             None
         """
-        # Generate color map
-        if len(self.population_proportions) > len(colors):
-            raise("Not enough colors to plot.")
-        else:
-            colors = colors[0:len(self.population_proportions)]
-            colors.insert(0, vacant_color)
-
-        # Display grid
-        plt.imshow(
-            self.grid,
-            cmap=ListedColormap(colors)
-        )
+        _, ax = plt.subplots(1, 2, figsize=(8, 4))
+        self._plot_grid(stage="initial", vacant_color=vacant_color, colors=colors, ax=ax[0])
+        self._plot_grid(stage="current", vacant_color=vacant_color, colors=colors, ax=ax[1])
+        ax[0].title.set_text("Initial")
+        ax[1].title.set_text(f"End (t = {self.t_end})")
+        plt.tight_layout()
         plt.show()
 
+    def grid_to_long_form(self, stage: str = "current") -> np.array:
+        """
+        Turn 2D population grid into long form: location (x, y) and value.
+
+        Parameters:
+            stage:  "current" or "initial" grid.
+
+        Return:
+            long_grid:  flattened population grid.
+        """
+        if stage == "current":
+            grid = self.grid
+        elif stage == "initial":
+            grid = self.grid_initial
+        x, y = np.meshgrid(np.arange(grid.shape[1]), np.arange(grid.shape[0]))
+        x_flat = x.ravel()
+        y_flat = y.ravel()
+        value_flat = grid.ravel()
+        long_grid = np.column_stack((x_flat, y_flat, value_flat))
+        return long_grid
 
